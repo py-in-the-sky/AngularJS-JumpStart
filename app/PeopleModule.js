8dc967a6a11bedc3746c2ee2b0d8cb58cc018235
Ryan Wise <ryanmwise1@gmail.com>
2014-04-13 16:48:34 -0700

    Adding some of the ng files

--- app/PeopleModule.js
+++ app/PeopleModule.js
@@ -9,19 +9,22 @@
             .when('/',
             {
                 controller: 'peopleController',
-                templateUrl: '/app/views/people.html'
+                templateUrl: 'app/views/people.html',
+                controllerAs: 'ctrl'  // replaces "peopleController as ctrl" view syntax
+                // see: http://sravi-kiran.blogspot.com/2013/11/ControllerAsSyntaxInAngularJs1.2.html
             })
-            .when('/person/:personName',
-            {
-                controller: 'personController',
-                templateUrl: '/app/views/person.html'
-            })
+            // .when('/person/:personName',
+            // {
+            //     controller: 'personController',
+            //     templateUrl: '/app/views/person.html',
+            //     controllerAs: 'ctrl2'
+            // })
             .otherwise({ redirectTo: '/' });
     };
 
     // tell Angular which objects are being injected, just in case minification changes the parameter names
     // order inside the list corresponds to the order of the parameters
-    myRouter.$inject = ['$routeProvider'];  // helps us avoid problems from minification
+    // myRouter.$inject = ['$routeProvider'];  // helps us avoid problems from minification
 
     // include the routes in a module
     peopleModule.config(myRouter);
